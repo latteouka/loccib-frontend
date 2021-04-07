d93f2b150187c08dc4a3976de0b11d6f870df1bf
oukalatte <oukalatte@gmail.com>
2021-04-07 13:26:27 +0800

    make it better

--- flask_app.py
+++ flask_app.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
-
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def hello():
