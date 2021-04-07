dfcb23440a0cdacde2a6d97368d065032f69ce05
oukalatte <oukalatte@gmail.com>
2021-04-07 13:18:46 +0800

    make it better

--- app.py
+++
@@ -1,8 +0,0 @@
-
-from flask import Flask, request, abort, render_template
-
-@app.route("/getname", methods=['GET'])
-
-def getname():
-    name = request.args.get('name')
-    return render_template('get.html',**locals())
