84603577980cdb44e05e3e2121d6b30be25e4978
oukalatte <oukalatte@gmail.com>
2021-04-09 12:21:18 +0800

    make it better

--- flask_app.py
+++ flask_app.py
@@ -30,6 +30,20 @@ def getname():
 
 @app.route("/addnew", methods=['GET'])
 def addnew():
+	# read parameters
+	header = request.args.get('header')
+	user = request.args.get('user')
+	msg = request.args.get('msg')
+	cell_lat = request.args.get('name')
+	cell_lon = request.args.get('name')
+	tri_lat = request.args.get('name')
+	tri_lon = request.args.get('name')
+	time = request.args.get('time')
+	stat = request.args.get('stat')
+
+	url_cell = "http://www.google.com.tw/maps/search/"+cell_lat+","+cell_lon
+	url_tri = "http://www.google.com.tw/maps/search/"+tri_lat+","+tri_lon
+
     connection = pymysql.connect(host=os.environ.get('CLEARDB_DATABASE_HOST'),
                              user=os.environ.get('CLEARDB_DATABASE_USER'),
                              password=os.environ.get('CLEARDB_DATABASE_PASSWORD'),
@@ -38,14 +52,20 @@ def addnew():
                              cursorclass=pymysql.cursors.DictCursor)
     with connection.cursor() as cursor:
         # add text value
-        sql = "INSERT INTO `records` (`header`, `user`, `msg`, `cell_lat`, `cell_lon`, `tri_lat`, `tri_lon`, `url`, `time`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
-        cursor.execute(sql, ('即時定位測試', 'chun', '訂位成功', '23', '123', '23', '123', 'http://ggfdg.com', '32432423432'))
+        sql = "INSERT INTO `records` (`header`, `user`, `msg`, `cell_lat`, `cell_lon`, `tri_lat`, `tri_lon`, `url`, `time`, `stat`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+        if tri_lat != 0:
+        	cursor.execute(sql, (header, user, msg, cell_lat, cell_lon, tri_lat, tri_lon, url_tri, 1))
+        else:
+        	cursor.execute(sql, (header, user, msg, cell_lat, cell_lon, tri_lat, tri_lon, url_cell, 0))
         cursor.close()
     connection.commit()
     return f'add new test record'
     
 @app.route("/show", methods=['GET'])
 def show():
+
+    user = request.args.get('chun')
+
     connection = pymysql.connect(host=os.environ.get('CLEARDB_DATABASE_HOST'),
                              user=os.environ.get('CLEARDB_DATABASE_USER'),
                              password=os.environ.get('CLEARDB_DATABASE_PASSWORD'),
@@ -55,7 +75,7 @@ def show():
 
     with connection.cursor() as cursor:
         sql = "SELECT * FROM `records` WHERE `user`=%s"
-        cursor.execute(sql, ('chun',))
+        cursor.execute(sql, (user,))
         results = cursor.fetchall()
         #print(result)
         cursor.close()
