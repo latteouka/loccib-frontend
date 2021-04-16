00644078af7daabeea0728f0c598707ce29804a4
oukalatte <oukalatte@gmail.com>
2021-04-16 22:36:51 +0800

    make it better

--- flask_app.py
+++ flask_app.py
@@ -459,7 +459,7 @@ def whois():
             ips.append(row[0])
             print(row[0])
             #彙整時間
-            time_format = datetime.strptime(row[1], "%Y%m%d%H%M")
+            time_format = datetime.datetime.strptime(row[1], "%Y%m%d%H%M")
             times.append(time)
             #彙整port
             ports.append(row[2])
@@ -469,7 +469,7 @@ def whois():
             ips.append(row[0])
             print(row[0])
             #彙整時間
-            time_format = datetime.strptime(row[1], "%Y%m%d%H%M")
+            time_format = datetime.datetime.strptime(row[1], "%Y%m%d%H%M")
             times.append(time)
             #彙整port
             ports.append('0')
