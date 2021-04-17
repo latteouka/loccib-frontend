97bb3e734abf68c6eeb7d0028d7324c8da60f189
oukalatte <oukalatte@gmail.com>
2021-04-17 17:48:40 +0800

    make it better

--- static/oneline.js
+++ static/oneline.js
@@ -67,3 +67,20 @@ function getresult_3() {
 
   document.getElementById("result").value = result_string;
 }
+
+function getresult_4() {
+  
+  var input = document.getElementById("input").value;
+  var inputs = input.split('\n');
+  
+  result_string = ""
+
+  for (i = 0; i < inputs.length; i++) { 
+    result_string = result_string + inputs[i]
+  }
+
+  //delete \n
+  result_string = result_string.slice(0, -1)
+
+  document.getElementById("result").value = result_string;
+}
