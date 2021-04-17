cec62688017839eefbc8cd866963f31415efdfbc
oukalatte <oukalatte@gmail.com>
2021-04-17 16:57:19 +0800

    make it better

--- static/combine.js
+++ static/combine.js
@@ -2,6 +2,21 @@
 document.getElementById("input").value = "text1\ntext2\ntext3";
 
 
+function clickcopy() {
+  /* Get the text field */
+  var copyText = document.getElementById("result");
+
+  /* Select the text field */
+  copyText.select(); 
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+  /* Copy the text inside the text field */
+  document.execCommand("copy");
+
+  /* Alert the copied text */
+  alert("Copied!!");
+}
+
 function getresult_1() {
   
   var input = document.getElementById("input").value;
